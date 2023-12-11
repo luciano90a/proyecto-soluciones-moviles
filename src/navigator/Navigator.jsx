ea39e4dbc20ff62c9742ac2d9b6a5d1955f1f65d
CristobalOssesO <cristobal.osses@hotmail.com>
2023-12-11 19:47:54 -0300

    Update Navigator

--- src/navigator/Navigator.jsx
+++ src/navigator/Navigator.jsx
@@ -1,41 +1,35 @@
-import {View,text} from 'react-native';
-import React ,{useContext}from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+/* eslint-disable prettier/prettier */
+import {View, text} from 'react-native';
+import React, {useContext} from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from '../views/Login';
 import Register from '../views/Register';
 import {Home} from '../views/Home';
 import Post from '../views/Post';
 import Profile from '../views/Profile';
 
-import { Authcontext } from '../context/Authcontext';
-const Stack= createNativeStackNavigator();
-export const Navigator= ()=>{
-    const { status } = useContext(Authcontext);
-    
+import {Authcontext} from '../context/Authcontext';
+const Stack = createNativeStackNavigator();
+export const Navigator = () => {
+  const {status} = useContext(Authcontext);
 
-return (
-  <Stack.Navigator
-    screenOptions={{
-      headerShown: false,
-    }}
-  >
-   
-    {status == 'noauth' ? (
-      <>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-      </>
-    ) 
-    : (
-      <>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Post" component={Post} />
-        <Stack.Screen name="Profile" component={Profile} />
-        
-
-      </>
-    )}
-  </Stack.Navigator>
-);
-
-}
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      {status == 'noauth' ? (
+        <>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Post" component={Post} />
+          <Stack.Screen name="Profile" component={Profile} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+};
